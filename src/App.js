6e23e5772317a0eaad0c97492ab831ee5c05f513
Thiago Krempser <tkrempser@gmail.com>
2021-06-22 23:08:31 -0300

    Update expense item component + Add expense date component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      amount: 9.99,
+      date: new Date(2021, 5, 22),
+      title: "Book",
+    },
+    {
+      id: "e2",
+      amount: 39.49,
+      date: new Date(2021, 5, 21),
+      title: "Shirt",
+    },
+    {
+      id: "e3",
+      amount: 85.59,
+      date: new Date(2021, 4, 8),
+      title: "Electricity bill",
+    },
+  ];
+
   return (
     <div className="App">
-      <ExpenseItem />
+      <ExpenseItem
+        amount={expenses[0].amount}
+        date={expenses[0]["date"]}
+        title={expenses[0].title}
+      />
+      <ExpenseItem
+        amount={expenses[1].amount}
+        date={expenses[1]["date"]}
+        title={expenses[1].title}
+      />
+      <ExpenseItem
+        amount={expenses[2].amount}
+        date={expenses[2]["date"]}
+        title={expenses[2].title}
+      />
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,13 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 5, 22);
-  const expenseTitle = "New book";
-  const expensePrice = 19.99;
-
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toDateString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">{expensePrice}</div>
+        <ExpenseDate date={props.date} />
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
