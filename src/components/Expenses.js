963417b37927423b49dc7d7c381264671473c81d
Thiago Krempser <tkrempser@gmail.com>
2021-06-23 12:40:24 -0300

    Add card reusable wrapper component

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,13 +1,14 @@
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 import "./Expenses.css";
 
 function Expenses(props) {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       {props.expenses.map((item) => (
         <ExpenseItem amount={item.amount} date={item.date} title={item.title} />
       ))}
-    </div>
+    </Card>
   );
 }
 
