76d2b56cd480af4fbfb3c0c7ccd41b6f1b45de51
Thiago Krempser <tkrempser@gmail.com>
2021-06-24 18:00:12 -0300

    Update expense filter component

--- src/components/ExpenseFilter/ExpenseFilter.js
+++ src/components/ExpenseFilter/ExpenseFilter.js
@@ -9,7 +9,7 @@ const ExpenseFilter = (props) => {
     <div className="expenses-filter">
       <div className="expenses-filter__control">
         <label>Filter by year</label>
-        <select onChange={changeHandler}>
+        <select onChange={changeHandler} value={props.selectedYear}>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,15 +2,19 @@ import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpenseFilter from "../ExpenseFilter/ExpenseFilter";
+import { useState } from "react";
 
 const Expenses = (props) => {
+  const [selectedYear, setSelectedYear] = useState("2021");
+
   const selectYearHandler = (selectedYear) => {
+    setSelectedYear(selectedYear);
     console.log(selectedYear);
   };
 
   return (
     <Card className="expenses">
-      <ExpenseFilter onSelectYear={selectYearHandler} />
+      <ExpenseFilter onSelectYear={selectYearHandler} selectedYear={selectedYear} />
       {props.expenses.map((item) => (
         <ExpenseItem
           key={item.id}
