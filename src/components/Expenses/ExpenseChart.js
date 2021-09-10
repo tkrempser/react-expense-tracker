097558850599082ff5bcfb27ba4c7234c99e2c16
Thiago Krempser <tkrempser@gmail.com>
2021-09-09 23:12:36 -0300

    Fix typos

--- src/components/Expenses/ExpenseChart.js
+++ src/components/Expenses/ExpenseChart.js
@@ -3,13 +3,13 @@ import Chart from "../Chart/Chart";
 const ExpenseChart = (props) => {
   let expensesDataPoint = [
     { label: "Jan", value: 0 },
-    { label: "Fev", value: 0 },
+    { label: "Feb", value: 0 },
     { label: "Mar", value: 0 },
     { label: "Apr", value: 0 },
     { label: "May", value: 0 },
     { label: "Jun", value: 0 },
     { label: "Jul", value: 0 },
-    { label: "Ago", value: 0 },
+    { label: "Aug", value: 0 },
     { label: "Sep", value: 0 },
     { label: "Oct", value: 0 },
     { label: "Nov", value: 0 },
