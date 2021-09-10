2171a97d3e4f9f067959df233f3b677ecd194714
Thiago Krempser <tkrempser@gmail.com>
2021-09-10 16:23:39 -0300

    Update expense item component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,24 +1,14 @@
-import { useState } from "react";
-
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle("Updated");
-  };
-
   return (
     <Card className="expense-item">
       <div className="expense-item__description">
         <ExpenseDate date={props.date} />
-        <h2>{title}</h2>
-        <div className="expense-item__price" onClick={clickHandler}>
-          ${props.amount.toFixed(2)}
-        </div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount.toFixed(2)}</div>
       </div>
     </Card>
   );
